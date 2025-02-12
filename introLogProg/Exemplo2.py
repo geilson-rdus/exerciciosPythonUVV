7e9150d20327ce8e622bc2adb1bc484f49224cdc
Geilson Lucas <geilson.gears@outlook.com>
2025-02-12 09:47:31 -0300

    Create Exemplo2.py

---
+++ introLogProg/Exemplo2.py
@@ -0,0 +1,8 @@
+'''
+Calcula a médias das notas do aluno no semestre
+'''
+
+n1 = float(input('Nota no primeiro bimestre: '))
+n2 = float(input('Nota no segundo bimestre: '))
+
+print(f'A média semestral do aluno foi {(n1+n2)/2:.2f}')
