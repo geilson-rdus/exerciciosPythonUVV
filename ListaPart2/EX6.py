944defc7223b485ecc823656e6331f5e193aa09e
Geilson Lucas <geilson.gears@outlook.com>
2025-02-19 16:04:31 -0300

    Create EX6.py

---
+++ ListaPart2/EX6.py
@@ -0,0 +1,15 @@
+# Faça um algoritmo que leia um número positivo e exiba se seu quadrado é ímpar e múltiplo de 11.
+
+try: 
+    numero = int(input('Informe um número inteiro e positivo'))
+    
+    if(numero <= 0):
+        print('ERRO: Dados de Entrada')
+    else:
+        quadrado = numero ** 2
+        if(quadrado % 2 != 0 and quadrado % 11 == 0):
+            print(f'O número digitado foi {numero}. Seu quadrado é {quadrado},  que é ímpar e múltiplo de 11')
+        else:
+            print(f'O número digitado foi {numero}. Seu quadrado é {quadrado},  que não é ímpar ou não é múltiplo de 11')
+except:
+    print('ERRO: Dados de Entrada')
