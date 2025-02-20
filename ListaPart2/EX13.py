b02892efc2d7556f20e2a263688df874cffdc03d
Geilson Lucas <geilson.gears@outlook.com>
2025-02-20 16:25:04 -0300

    Create EX13.py

---
+++ ListaPart2/EX13.py
@@ -0,0 +1,13 @@
+try:
+    n1 = float(input('Insira um número real: '))
+    n2 = float(input('Insira um número real: '))
+    n3 = float(input('Insira um número real: '))
+
+    media = (n1 + n2 + n3) / 3
+    if(media < (10 * 11.52743) or media > (200 * 11.52743)):
+        cuboMedia = media ** 3
+        print(f'O cubo da média: {cuboMedia: .5f}')
+    else:
+        print('Os números não se adequaram as exigências')
+except:
+    print('ERRO: Entrada inválida! Digite um número válido para o valor.')
