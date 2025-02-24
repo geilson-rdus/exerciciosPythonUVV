80675b6b471e8d97da987110b0a4ffa0155350fc
Geilson Lucas <geilson.gears@outlook.com>
2025-02-24 14:54:12 -0300

    Create EX16.py

---
+++ ListaPart2/EX16.py
@@ -0,0 +1,21 @@
+# Escrever um algoritmo que leia três (3) valores reais quaisquer e distintos (teste e avise ao usuário
+# se os valores informados forem iguais) e exiba na saída a média dos dois maiores números lidos.
+
+try:
+    n1 = int(input('Informe o primeiro número real: '))
+    n2 = int(input('Informe o segundo número real: '))
+    n3 = int(input('Informe o terceiro número real: '))
+    if(n1==n2 or n1==n3 or n2==n3):
+        print('ERRO: Você informou números iguais')
+    else:
+        soma = 0
+        if(n1>n2 or n1>n3):
+            soma += n1
+        if(n2>n3 or n2>n1):
+            soma += n2
+        if(n3>n1 or n3>n2):
+            soma += n3
+        media = soma/2
+        print(f'A média entre os dois maiores números é {media:.2f}')
+except Exception as ERRO_EXCLUSAO:
+    print(f'ERRO: {ERRO_EXCLUSAO}')
