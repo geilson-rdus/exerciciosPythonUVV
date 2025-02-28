f5f1ac59e06833e59ecebfbb01efdfb2ca0acd21
Geilson Lucas <geilson.gears@outlook.com>
2025-02-28 08:26:16 -0300

    Update EX07.py

--- ListaPart2/EX07.py
+++ ListaPart2/EX07.py
@@ -5,18 +5,18 @@
 try: 
     preco = float(input('Informe o preço da mercadoria em reais: '))
     reajuste = float(input('Informe o reajuste em porcentagem: '))
-    tipoReajuste = input('Desconto ou Acréscimo (escreva sem acento): ')
+    tipoReajuste = input('Desconto(d) ou Acréscimo(a): ')
     
     if(preco < 0 or reajuste < 0 or reajuste > 100):
-        print('ERRO: Dados de Entrada')
+        print('ERRO: Valores de preço ou reajuste inválidos')
     else:
-        if(tipoReajuste.lower() == 'desconto'):
+        if(tipoReajuste.lower() == 'd'):
             precoAjuste = preco * (1 - (reajuste / 100))
             print(f'O preço informado foi R${preco: .2f}, com um desconto de {reajuste:.2f}%, resultou num preço de R${precoAjuste: .2f}')
-        elif(tipoReajuste.lower() == 'acrescimo'):
+        elif(tipoReajuste.lower() == 'a'):
             precoAjuste = preco * (1 + (reajuste / 100))
             print(f'O preço informado foi R${preco: .2f}, com um acréscimo de {reajuste:.2f}%, resultou num preço de R${precoAjuste: .2f}')
         else:
-            print('ERRO: Dados de Entrada')
-except:
-    print('ERRO: Dados de Entrada')
+            print('ERRO: Inseriu um valor não correspondente com as opções')
+except Exception as ERRO_EXCECAO:
+  print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
