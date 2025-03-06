24187042fb96726211bd6339dbbe6b14eb693790
Geilson Lucas <geilson.gears@outlook.com>
2025-03-06 15:28:28 -0300

    Update EX05.py

--- ListaPart2/EX05.py
+++ ListaPart2/EX05.py
@@ -1,21 +1,19 @@
 # Escrever um algoritmo em Python que exiba o público total (inteiro) de um jogo de futebol e forneça a arrecadação (R$: real) do jogo
 
 try: 
-    publicoTotal = int(input('Público total presente: '))
+    adultosInteira = int(input('Maiores de 18 que pagaram inteira: '))
     criancasMenos10 = int(input('Quantidade de crianças menores de 11 anos: '))
     criancasEntre11E17 = int(input('Quantidade de crianças entre 11 e 17 anos: '))
     adultosDoadores = int(input('Maiores de 18 anos que doaram 1 kg de alimento: '))
     valorIngresso = float(input('Valor inteiro do ingresso em reais: '))
     
-    publicoMeia = criancasMenos10 + criancasEntre11E17 + adultosDoadores
+    publicoMeia = criancasEntre11E17 + adultosDoadores
+    publicoTotal = publicoMeia + adultosInteira + criancasMenos10
     
-    if(publicoTotal < 0 or criancasMenos10 < 0 or criancasEntre11E17 < 0 or adultosDoadores < 0 or valorIngresso <= 0):
+    if(adultosInteira < 0 or criancasMenos10 < 0 or criancasEntre11E17 < 0 or adultosDoadores < 0 or valorIngresso <= 0):
         print('ERRO: Informou valores negativos')
-    elif(publicoMeia > publicoTotal):
-        print('ERRO: Informou valoresque resultaram num público pagante de meia entrada maior do que o público total')
     else:
-        publicoPagaInteira = publicoTotal - publicoMeia
-        arrecadacao = (criancasEntre11E17 * (valorIngresso/2)) + (adultosDoadores * (valorIngresso/2)) + (publicoPagaInteira * valorIngresso)
-        print(f'O público total foi {publicoTotal} e o a arrecadação foi R${arrecadacao: .2f}')
+        arrecadacao = (criancasEntre11E17 * (valorIngresso/2)) + (adultosDoadores * (valorIngresso/2)) + (adultosInteira * valorIngresso)
+        print(f'O público total foi de {publicoTotal} pessoas e a arrecadação foi R${arrecadacao: .2f}')
 except Exception as ERRO_EXCECAO:
   print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
